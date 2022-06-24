032a8c649209c54d28bf9f6bded8131f288ba43a
Aeonn <theodebauvais@protonmail.com>
2022-06-24 18:54:45 +0200

    Add some classes in transpiler.py

--- transpiler.py
+++ transpiler.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-from typing import Any, List
+from typing import Any, List, Optional
 from abc import ABC, abstractmethod
-from parser import StructDefNode
+from parser import *
 from utils import Token
 
 
@@ -19,16 +19,37 @@ class IntNumber:
         self.value = int(value_token.value)
 
 
+class UnaryOp:
+    def __init__(self, node: UnaryOpNode):
+        self.op: str = str(node.op_token.value)
+        self.value: Expression = Expression(node.node)
+
+
+class BinaryOp:
+    def __init__(self, node: BinOpNode):
+        self.left: Expression = Expression(node.left_node)
+        self.op: str = str(node.op_token.value)
+        self.right: Expression = Expression(node.right_node)
+
+
+class Expression:
+    # TODO
+    def __init__(self, node):
+        pass
+
+
 class StructMember:
-    def __init__(self, name_token: Token, type_token: Token, is_list: bool = False, list_len_expr: Any = 0) -> None:
+    def __init__(self, name_token: Token, type_token: Token, is_list: bool = False, list_len_node: Any = 0):
         self.name: str = str(name_token.value)
         self.type: str = str(type_token.value)
         self.is_list: bool = is_list
-        self.list_len_expr: Any = list_len_expr
+        self.list_len_expr: Optional[Expression] = None
+        if self.is_list:
+            self.list_len_expr = Expression(list_len_node)
 
 
 class Struct:
-    def __init__(self, name_token: Token) -> None:
+    def __init__(self, name_token: Token):
         self.name: str = str(name_token.value)
         self.members: List[StructMember] = []
 
