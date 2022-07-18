0ac69251b5a9c1d34a5775d921df0d6a0502f834
Aeonn <theodebauvais@protonmail.com>
2022-07-18 11:44:21 +0200

    Reworked python generator and added functionnalities.

--- generators/python_class.py
+++ generators/python_class.py
@@ -2,29 +2,68 @@
 from transpiler import *
 
 class Python_Class(ParseedOutputGenerator):
-    def generate(self) -> str:
-        datatype_struct_mapping = {
-            "uint8": "B",
-            "int8": "b",
-            "uint16": "H",
-            "int16": "h",
-            "uint32": "B",
-            "int32": "b",
-        }
+    def generate(self, writer):
 
-        res = ""
         for struct in self.structs:
-            res += "import struct\n"
-            res += f"class {struct.name}:\n"
-            for index, member in enumerate(struct.members):
-                res += f"\t{member.name} = None\n\n"
-            res += f"\tdef __init__(self, buf):\n"
-            res += f"\t\ttmp = struct.unpack('"
+            current_block = writer.add_block()
+
+            current_block.add_line("import struct")
+            current_block.add_empty_line()
+            current_block.add_line(f"class {struct.name}:")
+
+            current_block = current_block.add_block()
+            current_block.add_line(f"def __init__(self, buf):")
+
+            current_block = current_block.add_block()
+            current_block.add_line("curr_index = 0")
             for member in struct.members:
-                res += datatype_struct_mapping[member.type]
-            res += "')\n"
+                current_block.add_line(f"self.{member.name} = struct.unpack_from(\"{self.get_data_type_as_struct_format(member)}\", buf, curr_index)[0]")
+                current_block.add_line(f"curr_index += {DataType(member.type).size}")
+            current_block = current_block.end_block()
+            current_block.add_empty_line()
 
-            for index, member in enumerate(struct.members):
-                res += f"\t\tself.{member.name} = res[{index}]\n"
+            current_block.add_line("def __str__(self):")
+            current_block = current_block.add_block()
 
-        return res
+            current_block.add_line(f"res = \"{struct.name}(\" + \\")
+            current_block = current_block.add_block() # just to have a better indentation
+            for member in struct.members:
+                current_block.add_line(f"\"{member.name} = \" + str(self.{member.name}) + \"\\n\" \\")
+            current_block = current_block.end_block()
+            current_block.add_line(f"\")\"")
+            current_block.add_line(f"return res")
+
+    def get_data_type_as_struct_format(self, member):
+        dt = DataType(member.type)
+        if dt.is_byte():
+            return "c"
+        elif dt.is_string():
+            raise NotImplementedError("String is not yet implemented in Python_Class.")
+        elif dt.is_float():
+            return "f"
+        elif dt.is_double():
+            return "d"
+        else:
+            res = ""
+            if dt.endian == DataType.ENDIAN_BIG:
+                res += ">"
+            elif dt.endian == DataType.ENDIAN_LITTLE:
+                res += "<"
+            
+            if dt.size == 1:
+                res += "b"
+            elif dt.size == 2:
+                res += "h"
+            elif dt.size == 3:
+                raise NotImplementedError("String is not yet implemented in Python_Class.")
+            elif dt.size == 4:
+                res += "i"
+            elif dt.size == 5:
+                raise NotImplementedError("String is not yet implemented in Python_Class.")
+            elif dt.size == 6:
+                raise NotImplementedError("String is not yet implemented in Python_Class.")
+            elif dt.size == 8:
+                res += "q"
+            elif dt.size == 16:
+                raise NotImplementedError("String is not yet implemented in Python_Class.")
+            return res
