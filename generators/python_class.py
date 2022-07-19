0ac7facea5d09b0d27d6cb98984d3c644bdc605c
Aeonn <theodebauvais@protonmail.com>
2022-07-19 12:48:48 +0200

    refactored a bit python_class generator.

--- generators/python_class.py
+++ generators/python_class.py
@@ -13,13 +13,14 @@ class Python_Class(ParseedOutputGenerator):
 
             current_block = current_block.add_block()
             current_block.add_line(f"def __init__(self, buf):")
-
             current_block = current_block.add_block()
+
             current_block.add_line("curr_index = 0")
             for member in struct.members:
                 if member.is_list():
                     current_block.add_line(f"self.{member.name} = []")
                     current_block.add_line(f"for i in range(0, {member.list_length}):")
+
                     current_block = current_block.add_block()
                     current_block.add_line(f"self.{member.name}.append(struct.unpack_from(\"{self.get_data_type_as_struct_format(member)}\", buf, curr_index)[0])")
                     current_block.add_line(f"curr_index += {DataType(member.type).size}")
@@ -30,17 +31,21 @@ class Python_Class(ParseedOutputGenerator):
             current_block = current_block.end_block()
             current_block.add_empty_line()
 
-            # generate __str__ function
-            current_block.add_line("def __str__(self):")
-            current_block = current_block.add_block()
+            current_block = self.generate_str_function(current_block, struct)
 
-            current_block.add_line(f"res = \"{struct.name}(\" + \\")
-            current_block = current_block.add_block() # just to have a better indentation
-            for member in struct.members:
-                current_block.add_line(f"\"{member.name} = \" + str(self.{member.name}) + \"\\n\" \\")
-            current_block = current_block.end_block()
-            current_block.add_line(f"\")\"")
-            current_block.add_line(f"return res")
+    def generate_str_function(self, current_block, struct):
+        # generate __str__ function
+        current_block.add_line("def __str__(self):")
+        current_block = current_block.add_block()
+
+        current_block.add_line(f"res = \"{struct.name}(\" + \\")
+        current_block = current_block.add_block() # just to have a better indentation
+        for member in struct.members:
+            current_block.add_line(f"\"{member.name} = \" + str(self.{member.name}) + \"\\n\" \\")
+        current_block = current_block.end_block()
+        current_block.add_line(f"\")\"")
+        current_block.add_line(f"return res")
+        return current_block.end_block()
 
     def get_data_type_as_struct_format(self, member):
         # see: https://docs.python.org/3/library/struct.html#format-characters
