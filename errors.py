6dedf36a024f1befd78910a4507586eeb4d060e6
Aeonn <theodebauvais@protonmail.com>
2022-11-10 12:31:11 +0100

    Added docstrings in errors.py

--- errors.py
+++ errors.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python3
 from utils import *
+from typing import Union
 
 
 class ParseedBaseError(BaseException):
+    """
+    Base class for internal errors.
+    This class implements the _underline_error method that underline the text from a Position to another Position.
+    """
+
     def _underline_error(self, pos_start: Position, pos_end: Position) -> str:
+        """
+        Underline where the error occured.
+        Note that both position should have the same text (meaning: pos_start.file_text == pos_end.file_text).
+        :param post_start: Position where to start the underline.
+        :type post_start: Position
+        :param post_end: Position where to stop the underline.
+        :type post_end: Position
+        """
         res_text: List[str] = []
         res_underline: List[str] = []
 
@@ -40,7 +54,22 @@ class ParseedBaseError(BaseException):
 
 
 class ParseedSimpleUnderlinedError(ParseedBaseError):
+    """
+    Error with one line containing the error.
+    If there the error is present on multiple different lines, ParseedMultipleUnderlinedError should be used.
+    """
+
     def __init__(self, pos_start: Position, pos_end: Position, error_name: str, details: str):
+        """
+        :param pos_start: Start position of the error.
+        :type pos_start: Position
+        :param pos_end: End position of the error.
+        :type pos_end: Position
+        :param error_name: Name of the error.
+        :type error_name: str
+        :param details: Details about the error.
+        :type details: str
+        """
         self.pos_start: Position = pos_start
         self.pos_end: Position = pos_end
         self.error_name: str = error_name
@@ -51,8 +80,22 @@ class ParseedSimpleUnderlinedError(ParseedBaseError):
 
 
 class ParseedMultipleUnderlinedError(ParseedBaseError):
+    """
+    Error with multiple lines containing the error.
+    If there the error is present on only one line, ParseedSimpleUnderlinedError should be used.
+    """
+
     def __init__(self, pos_start: List[Position], pos_end: List[Position], error_name: str, details: str):
-        assert len(pos_start) == len(pos_end)
+        """
+        :param pos_start: Start positions of the error.
+        :type pos_start: List[Position]
+        :param pos_end: End positions of the error.
+        :type pos_end: List[Position]
+        :param error_name: Name of the error.
+        :type error_name: str
+        :param details: Details about the error.
+        :type details: str
+        """
         self.pos_start: List[Position] = pos_start
         self.pos_end: List[Position] = pos_end
         self.error_name: str = error_name
@@ -69,42 +112,118 @@ class ParseedMultipleUnderlinedError(ParseedBaseError):
 
 
 class IllegalCharacterError(ParseedSimpleUnderlinedError):
+    """
+    Should be raised when a character was not expected.
+    """
+
     def __init__(self, pos_start: Position, pos_end: Position, details: str):
+        """
+        :param pos_start: Start position of the error.
+        :type pos_start: Position
+        :param pos_end: End position of the error.
+        :type pos_end: Position
+        :param details: Details about the error.
+        :type details: str
+        """
         super().__init__(pos_start, pos_end, "Illegal character error", f"{details}, col {pos_start.col}")
 
 
 class InvalidSyntaxError(ParseedSimpleUnderlinedError):
+    """
+    Should be raised when the syntax is invalid (thrown by the parser).
+    """
+
     def __init__(self, pos_start: Position, pos_end: Position, details: str):
+        """
+        :param pos_start: Start position of the error.
+        :type pos_start: Position
+        :param pos_end: End position of the error.
+        :type pos_end: Position
+        :param details: Details about the error.
+        :type details: str
+        """
         super().__init__(pos_start, pos_end, "Invalid syntax error", details)
 
 
 class ExpectedMoreCharError(ParseedSimpleUnderlinedError):
+    """
+    Should be raised when some characters where expected but not found.
+    """
+
     def __init__(self, pos_start: Position, pos_end: Position, expected_chars: List[str]):
+        """
+        :param pos_start: Start position of the error.
+        :type pos_start: Position
+        :param pos_end: End position of the error.
+        :type pos_end: Position
+        :param expected_chars: List of characters expected.
+        :type expected_chars: List[str]
+        """
         list_chars: str = ", ".join([f"'{c}'" for c in expected_chars])
         super().__init__(pos_start, pos_end, "Expected more chars error", list_chars)
 
 
 class UnknownTypeError(ParseedSimpleUnderlinedError):
+    """
+    Sould be raised when an unkown data-type is found and no bitfield or struct have the same name.
+    """
+
     def __init__(self, pos_start: Position, pos_end: Position, type_name: str, struct_name: str):
+        """
+        :param pos_start: Start position of the error.
+        :type pos_start: Position
+        :param pos_end: End position of the error.
+        :type pos_end: Position
+        :param type_name: Name of the unknown type.
+        :type type_name: str
+        :param struct_name: Name of the struct where the type was found.
+        :type struct_name: str
+        """
         super().__init__(pos_start, pos_end, "Unknown data type error", f"\"{type_name}\" in struct {struct_name}")
 
 
 class RecursiveStructError(ParseedMultipleUnderlinedError):
+    """
+    Should be raised when a struct includes itself (by a member or sub-member of any depth).
+    """
+
     def __init__(self, structs):
+        """
+        :param structs: List of structs where the recursion occurs.
+        :type structs: List[StructDefNode]
+        """
         pos_start: List[Position] = [s._name_token.pos_start for s in structs]
         pos_end: List[Position] = [s._name_token.pos_end for s in structs]
         super().__init__(pos_start, pos_end, "Recursive nested structs error", " -> ".join([s.name for s in structs]))
 
 
 class DuplicateMemberError(ParseedMultipleUnderlinedError):
-    def __init__(self, members, struct_name: str):
+    """
+    Should be raised when multiple members in the same bifield or struct share the same name.
+    """
+
+    def __init__(self, members, struct_or_bitfield_name: str):
+        """
+        :param members: Members sharing the same name.
+        :type members: List[Union[StructDefNode, BitfieldDefNode]]
+        :param struct_or_bitfield_name: Name of the struct or bitfield containing the duplicated members.
+        :type struct_or_bitfield_name: str
+        """
         pos_start: List[Position] = [m._name_token.pos_start for m in members]
         pos_end: List[Position] = [m._name_token.pos_end for m in members]
-        super().__init__(pos_start, pos_end, "Duplicate member error", members[0].name)
+        super().__init__(pos_start, pos_end, f"Duplicate member error", struct_or_bitfield_name)
 
 
 class DuplicateStructOrBitfieldError(ParseedMultipleUnderlinedError):
+    """
+    Should be raised when multiple structs or bitfield share the same name.
+    """
+
     def __init__(self, nodes):
+        """
+        :param nodes: List of structs and bitfields sharing the same name.
+        :type nodes: Union[StructDefNode, BitfieldDefNode]
+        """
         pos_start: List[Position] = [n._name_token.pos_start for n in nodes]
         pos_end: List[Position] = [n._name_token.pos_end for n in nodes]
         super().__init__(pos_start, pos_end, "Duplicate struct or bitfield error", nodes[0].name)
@@ -114,5 +233,6 @@ class InvalidStateError(Exception):
     """
     This error is used in the parser to indicate some intern invalid state,
     but not an error from the code.
+    It should be captured internally.
     """
     pass
